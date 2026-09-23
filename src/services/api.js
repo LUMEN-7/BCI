f69546d524a85199d8c18e1885d7745ea7f0e2b5
Ggmerda <eduardinhoggsouza@gmail.com>
2026-09-22 23:57:16 -0300

    arrunado url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://apiford.onrender.com";
-const API_BASE_URL = "https://localhost:7213";
+const API_BASE_URL = "https://apiford.onrender.com";
+// const API_BASE_URL = "https://localhost:7213";
 
 function erro401(response, skipRedirect = false){
   if (response.status === 401 && !skipRedirect) {
